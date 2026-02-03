ea288d9b411c0e0be2c63fd9e4812254dd96f075
Shiv Kant <shivkant639624@gmail.com>
2026-02-03 17:58:21 +0000

    Email config and Contact Page created

---
+++ src/config/email.js
@@ -0,0 +1,5 @@
+export const EMAIL_CONFIG = {
+  serviceId: import.meta.env.VITE_EMAIL_SERVICE,
+  templateId: import.meta.env.VITE_EMAIL_TEMPLATE,
+  publicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+};
